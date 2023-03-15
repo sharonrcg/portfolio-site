71c21a63af391545192845a900f53bfa03614ce5
sharonrgomez <sharonrachelg@gmail.com>
2023-03-15 16:59:34 -0400

    refine scroll snapping

--- src/hooks/useScrollSnap.ts
+++ src/hooks/useScrollSnap.ts
@@ -1,5 +1,7 @@
 import {useRef, useEffect} from 'react'
 import createScrollSnap from 'scroll-snap'
+const easeInOutCubic = (t: number) =>
+	t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
 
 export const useScrollSnap = () => {
 	const containerRef = useRef<HTMLElement>(null)
@@ -10,9 +12,10 @@ export const useScrollSnap = () => {
 		const element = containerRef.current
 
 		const {unbind} = createScrollSnap(element, {
-			duration: 500,
+			duration: 300,
 			snapDestinationY: '100%',
-			timeout: 50,
+			easing: easeInOutCubic,
+			threshold: 0.1,
 		})
 
 		return () => {
