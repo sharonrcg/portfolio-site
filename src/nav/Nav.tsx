04386ce1b8cb24a880ab06d213b45a5de47bbb3e
sharonrgomez <sharonrachelg@gmail.com>
2023-03-19 15:54:01 -0400

    hamburger menu!!

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -8,30 +8,54 @@ import styles from '@/styles/Nav.module.scss'
 const Nav = () => {
 	const {isDarkMode} = useContext(ThemeContext)
 	const {activeNavLinkId} = useContext(NavContext)
+	const [isMenuOpen, setIsMenuOpen] = React.useState(false)
 
 	React.useEffect(() => {
 		const homeSection = document.getElementById('homeContainer')
 		homeSection?.scrollIntoView({behavior: 'smooth'})
 	}, [])
 
+	const handleMenuClick = () => {
+		setIsMenuOpen(!isMenuOpen)
+	}
+
+	const renderNavLinks = () => {
+		return navLinks.map(({navLinkId, scrollToId}, idx) =>
+			scrollToId ? (
+				<NavLink
+					key={idx}
+					navLinkId={navLinkId}
+					scrollToId={scrollToId}
+					className={styles.activeClass}
+				/>
+			) : null,
+		)
+	}
+
 	return (
-		<nav
-			className={`${styles.Nav} ${isDarkMode ? styles.dark : ''} ${
-				styles[activeNavLinkId]
-			}`}
-		>
-			{navLinks.map(({navLinkId, scrollToId}, idx) =>
-				scrollToId ? (
-					<NavLink
-						key={idx}
-						navLinkId={navLinkId}
-						scrollToId={scrollToId}
-						className={styles.activeClass}
-					/>
-				) : null,
+		<>
+			<nav
+				className={`${styles.Nav} ${isDarkMode ? styles.dark : ''} ${
+					styles[activeNavLinkId]
+				}`}
+			>
+				{renderNavLinks()}
+				<ThemeButton />
+			</nav>
+
+			<nav className={styles.menuButton}>
+				<button onClick={handleMenuClick}>☰</button>
+			</nav>
+
+			{isMenuOpen && (
+				<nav className={`${styles.popover} ${styles[activeNavLinkId]}`}>
+					<ul className={styles.sectionsList}>
+						{renderNavLinks()}
+						{/* <ThemeButton /> */}
+					</ul>
+				</nav>
 			)}
-			<ThemeButton />
-		</nav>
+		</>
 	)
 }
 
