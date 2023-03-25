427072c816052684fbecfe5af84bd1fa8b2ea633
sharonrgomez <sharonrachelg@gmail.com>
2023-03-25 00:30:51 -0400

    fix dark mode polaroids

--- src/sections/About/components/PolaroidStack.tsx
+++ src/sections/About/components/PolaroidStack.tsx
@@ -1,5 +1,7 @@
 import {Polaroid} from './Polaroid'
 import styles from '@/styles/PolaroidStack.module.scss'
+import {ThemeContext} from '@/context'
+import {useContext} from 'react'
 
 type PolaroidStackProps = {
 	polaroids: {
@@ -11,9 +13,10 @@ type PolaroidStackProps = {
 
 export const PolaroidStack = (props: PolaroidStackProps) => {
 	const {polaroids} = props
+	const {isDarkMode} = useContext(ThemeContext)
 
 	return (
-		<div className={styles.PolaroidStack}>
+		<div className={`${styles.PolaroidStack} ${isDarkMode ? styles.dark : ''}`}>
 			{polaroids.length > 0 &&
 				polaroids.map((polaroid, idx) => (
 					<Polaroid
