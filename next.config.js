767dea865f752382ed074c5e4ea033677e7b54ce
Sharon <sharonrachelg@gmail.com>
2023-03-26 21:36:02 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	async rewrites() {
+		return {
+			beforeFiles: [
+				{
+					source: '/',
+					destination: 'https://gamerank-u3lo.onrender.com/',
+					has: [
+						{
+							type: 'host',
+							value: 'gamerank.sharongomez.com',
+						},
+					],
+				},
+			],
+		}
+	},
 }
 
 module.exports = nextConfig
